3fbd251fbe8d814b6ef2455a9d48ef807fff5b2c
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-11-16 18:57:19 +0100

    Add not null constraints

--- spring-server/src/main/java/io/lozzikit/users/entities/UserEntity.java
+++ spring-server/src/main/java/io/lozzikit/users/entities/UserEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Entity
@@ -13,10 +14,19 @@ public class UserEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
     private String username;
+
+    @NotNull
     private String firstName;
+
+    @NotNull
     private String lastName;
+
+    @NotNull
     private String email;
+
+    @NotNull
     private String password;
 
     public long getId() {
