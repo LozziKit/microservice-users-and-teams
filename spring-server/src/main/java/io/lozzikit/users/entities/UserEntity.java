20ec4bc89cfe320924ef15c951f52d9bb5deb71f
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-11-17 12:38:14 +0100

    handle min chars from post request

--- spring-server/src/main/java/io/lozzikit/users/entities/UserEntity.java
+++ spring-server/src/main/java/io/lozzikit/users/entities/UserEntity.java
@@ -4,7 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -14,19 +16,24 @@ public class UserEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotNull
+    /*@NotNull
+    @Size(min=4)*/
     private String username;
 
-    @NotNull
+    /*@NotNull
+    @Size(min=2)*/
     private String firstName;
 
-    @NotNull
+    //@NotNull
+    //@Size(min=2)
     private String lastName;
 
-    @NotNull
+    //@NotNull
+    //@Size(min=6)
     private String email;
 
-    @NotNull
+    //@NotNull
+    //@Size(min=6)
     private String password;
 
     public long getId() {
