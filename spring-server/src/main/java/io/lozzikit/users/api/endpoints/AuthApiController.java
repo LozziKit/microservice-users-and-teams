35545885579d5e6c0ca404bec4738a33790cff40
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-11-23 19:17:25 +0100

    Add authentification // remaining: refractoring to transform controller treatment into services; Send a real JWT to the user after the authentification check pass

---
+++ spring-server/src/main/java/io/lozzikit/users/api/endpoints/AuthApiController.java
@@ -0,0 +1,47 @@
+package io.lozzikit.users.api.endpoints;
+
+import io.lozzikit.users.api.AuthApi;
+import io.lozzikit.users.api.UsersApi;
+import io.lozzikit.users.api.model.Credentials;
+import io.lozzikit.users.api.model.Token;
+import io.lozzikit.users.entities.UserEntity;
+import io.lozzikit.users.api.model.User;
+import io.lozzikit.users.api.model.NewUser;
+import io.lozzikit.users.repositories.UserRepository;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-07-26T19:36:34.802Z")
+
+@Controller
+public class AuthApiController implements AuthApi {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public ResponseEntity<Token> authUser(@ApiParam(value = "", required = true) @Valid @RequestBody Credentials credentials) {
+        UserEntity user = userRepository.findByUsername(credentials.getUsername());
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        if(user != null && passwordEncoder.matches(credentials.getPassword(), user.getPassword())){
+            String message = "here is your token!";
+            return ResponseEntity.ok(new Token());
+        }
+        else{
+            return new ResponseEntity<Token>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
