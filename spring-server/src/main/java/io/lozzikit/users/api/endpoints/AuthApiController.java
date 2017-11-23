c21f052bb631de887b3660cfa83405c362393a02
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-11-23 23:28:49 +0100

    Clean imports in AuthApiController.java

--- spring-server/src/main/java/io/lozzikit/users/api/endpoints/AuthApiController.java
+++ spring-server/src/main/java/io/lozzikit/users/api/endpoints/AuthApiController.java
@@ -1,29 +1,20 @@
 package io.lozzikit.users.api.endpoints;
 
 import io.lozzikit.users.api.AuthApi;
-import io.lozzikit.users.api.UsersApi;
 import io.lozzikit.users.api.model.Credentials;
 import io.lozzikit.users.api.model.Token;
 import io.lozzikit.users.entities.UserEntity;
-import io.lozzikit.users.api.model.User;
-import io.lozzikit.users.api.model.NewUser;
-import io.lozzikit.users.repositories.UserRepository;
 import io.lozzikit.users.service.UserService;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-07-26T19:36:34.802Z")
 
