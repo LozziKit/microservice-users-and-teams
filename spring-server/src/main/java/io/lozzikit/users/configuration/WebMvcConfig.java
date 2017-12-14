4e6d1de73faa416bd7cac775f82f7facb7d3563f
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-12-14 12:04:02 +0100

    Remove useless code

--- spring-server/src/main/java/io/lozzikit/users/configuration/WebMvcConfig.java
+++ spring-server/src/main/java/io/lozzikit/users/configuration/WebMvcConfig.java
@@ -3,11 +3,9 @@ package io.lozzikit.users.configuration;
 import io.lozzikit.users.api.security.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
-@EnableWebMvc
+
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
@@ -17,16 +15,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry
             .addInterceptor(authenticationInterceptor)
-            .addPathPatterns("/**")
-            .excludePathPatterns(
-                "/",
-                "/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/**",
-                "/swagger-ui.html",
-                "/webjars/**",
-                "/api-docs**"
-            );
+            .addPathPatterns("/**");
     }
 }
