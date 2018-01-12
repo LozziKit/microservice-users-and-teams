2f9c4d01254466755a1f9cedb82348aeb4cd0551
Daniel Palumbo <dpalumbo06@gmail.com>
2018-01-12 22:11:08 +0100

    build and run docker images with script

--- build-image.sh
+++ build-image.sh
@@ -4,16 +4,22 @@ echo "----- Building server JAR ------"
 cd spring-server/
 mvn clean install
 
-echo "----- Copying JAR -----"
+echo "----- Copying JAR and files -----"
+rm ../images/server/server.jar
 cp target/microservice-users-and-teams-server-1.0.0.jar ../images/server/server.jar
+
+rm -r ../images/tests/users-and-teams-specs/
+mkdir ../images/tests/users-and-teams-specs/
 cp -r ../users-and-teams-specs/ ../images/tests/
 
-cd ../images/
+echo "----- Building image database ------"
+cd ../database
+docker build -t lozzikit/users-and-teams/database .
 
-echo "----- Building image ------"
+echo "----- Building image server ------"
+cd ../images/server
 docker build -t lozzikit/users-and-teams/server .
 
-cd ../database
-
-echo "----- Building image ------"
-docker build -t lozzikit/users-and-teams/database .
+echo "----- Building image tests ------"
+cd ../tests
+docker build -t lozzikit/users-and-teams/tests .
