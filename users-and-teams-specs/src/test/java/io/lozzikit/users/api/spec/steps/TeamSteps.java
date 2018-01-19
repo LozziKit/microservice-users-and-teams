ac98e8168550f38104c96b9a2fdda4da6bb1ed9f
Viking & Robot <vikingandrobot@gmail.com>
2018-01-19 19:06:04 +0100

    Added get teams step.

--- users-and-teams-specs/src/test/java/io/lozzikit/users/api/spec/steps/TeamSteps.java
+++ users-and-teams-specs/src/test/java/io/lozzikit/users/api/spec/steps/TeamSteps.java
@@ -11,6 +11,7 @@ import io.lozzikit.users.api.UserApi;
 import io.lozzikit.users.api.spec.helpers.Environment;
 import io.lozzikit.users.api.dto.NewTeam;
 import io.lozzikit.users.api.dto.User;
+import io.lozzikit.users.api.dto.Team;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +44,8 @@ public class TeamSteps {
     private ApiResponse lastApiResponse;
 
     private String location = null;
+
+    List<Team> receivedTeams = null;
 
 
     public TeamSteps(Environment environment, ApiSteps apiSteps, UpdateSteps updateSteps) {
@@ -105,5 +108,101 @@ public class TeamSteps {
     @Then("^I haven't received the team location$")
     public void i_ve_not_received_the_team_location() throws Throwable {
         assertNull(location);
+    }
+
+    @When("^I GET teams from the /team endpoint$")
+    public void i_GET_teams_from_the_team_endpoint() throws Throwable {
+        try {
+            ApiResponse<List<Team>> response = teamApi.getTeamsWithHttpInfo();
+            apiSteps.setLastApiException(null);
+            apiSteps.setLastStatusCode(response.getStatusCode());
+            receivedTeams = response.getData();
+            lastApiResponse = response;
+        } catch (ApiException e) {
+            apiSteps.setLastApiException(e);
+        }
+    }
+
+    @Then("^I've received a list of teams$")
+    public void i_ve_received_a_list_of_teams() throws Throwable {
+        assertNotNull(receivedTeams);
+    }
+
+    @Given("^I have a valid teamId$")
+    public void i_have_a_valid_teamId() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I GET team from the /team endpoint$")
+    public void i_GET_team_from_the_team_endpoint() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I've received a team payload$")
+    public void i_ve_received_a_team_payload() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I don't have a valid teamId$")
+    public void i_don_t_have_a_valid_teamId() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I'haven't received a team payload$")
+    public void i_haven_t_received_a_team_payload() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I have a valid teamPayload$")
+    public void i_have_a_valid_teamPayload() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I PUT it from the /team endpoint$")
+    public void i_PUT_it_from_the_team_endpoint() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I've received the team update payload$")
+    public void i_ve_received_the_team_update_payload() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I don't have a valid teamPayload$")
+    public void i_don_t_have_a_valid_teamPayload() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I haven't received the team update payload$")
+    public void i_haven_t_received_the_team_update_payload() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I update name of team with valid teamName$")
+    public void i_update_name_of_team_with_valid_teamName() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I'v received the team update payload$")
+    public void i_v_received_the_team_update_payload() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I update the name of team and is already exists$")
+    public void i_update_the_name_of_team_and_is_already_exists() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
